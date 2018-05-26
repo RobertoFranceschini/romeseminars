047e8396c2baf6fec31a3b85bc2c399a0ee0a788
Roberto <foice.news@gmail.com>
2018-05-26 19:43:02 +0200

    first commit

---
+++ cal_maker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from urllib.parse import quote
+import json
+import ijson # should be able to read only the values you need
+#import os
+
+calendar_json = "calendars.json"
+
+color_map={}
+#
+color_map['rm1']='BE6D00'
+color_map['rm2']='528800'
+color_map['rm3']='2952A3'
+color_map['lnf']='AB8B00'
+
+others=''
+with open(calendar_json, 'r') as f:
+    for line in f:
+        if line[0] != '#':
+            calendar_data = json.loads(line) #it is now a dictionary
+            others+='src='+calendar_data['id']+'&amp;color=%23'+color_map[calendar_data['color']]+'&amp;'
+
+title=quote('Rome Physics Seminars (Theoretical Physics)')
+#print(title)
+#print('\n')
+
+srcstring="https://calendar.google.com/calendar/embed?title="+title+"&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;hl=en&amp;bgcolor=%23FFFFFF&amp;"+others+'ctz=Europe%2FRome"'
+
+iframe='<iframe src="'+srcstring+' style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>'
+
+print(iframe)
