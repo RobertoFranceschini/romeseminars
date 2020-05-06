db38e9b594344157d62f0c9a35e642d7c160aed3
foice <foice.news@gmail.com>
2020-05-06 11:30:46 +0200

    More clear usage message

--- cal_maker.py
+++ cal_maker.py
@@ -6,15 +6,17 @@ import ijson # should be able to read only the values you need
 import argparse
 from flask_table import Table, Col
 
+_usage=
 '''
-Output gives two outputs, one above and one below the \%\%\%\%\%\%\%\% (for best results is advisable to redirect its output to file to no mess up newlines, that is.
+Output gives three outputs, separated by \%\%\%\%\%\%\%\% (for best results is advisable to redirect its output to file to no mess up newlines, that is.
 cal_maker.py > topaste. Then you can open `topaste` in a text editor and copy the text)
-copy the output above the \%\%\%\%\% in the webpage editor. You have to edit the raw HTML and replace the old code for the calendar widget. Beware that the code you paste does not look like the one you are going to paste in, because google process what you paste and transforms it in the actual widget.
-copy the output below the \%\%\%\%\% to redefine the cals_json array in the google script (you can find it from https://script.google.com/home/ pay attention that calendars ID must use @ and not \%40, cal_maker.py should make the change automatically but please check
+- copy the `FIRST OUTPUT BLOCK` above the \%\%\%\%\% in the Google Site webpage editor. You have to edit the raw HTML and replace the old code for the `calendar widget`. Beware that the code you paste does not look like the one you are going to paste in, because google process what you paste and transforms it in the actual widget.
+- copy the second output below the \%\%\%\%\% to make the list of Names of the semianrs series to pase as regular HTML in the Google Site webpage editor
+- (optional: if you have a script that sends emails with the summary of the next week) copy the third output below the \%\%\%\%\% to redefine the cals_json array in the google script (you can find it from https://script.google.com/home/ pay attention that calendars ID must use @ and not \%40, cal_maker.py should make the change automatically but please check
 '''
 
 
-parser = argparse.ArgumentParser(description='Process a JSON file to produce a valid Google Calendar widget and AppScript variables for the automatic generation of a PDF calendar.', usage='Copy')
+parser = argparse.ArgumentParser(description='Process a JSON file to produce a valid Google Calendar widget and Google AppScript variables for the automatic generation of a PDF calendar.', usage=_usage)
 
 
 parser.add_argument('--file',
@@ -83,12 +85,28 @@ srcstring="https://calendar.google.com/calendar/embed?title="+title+"&amp;mode=A
 
 iframe='<iframe src="'+srcstring+' style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>'
 
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('%%%%%%% WIDGET %%%%%%%%%')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 print(iframe)
-print('%%%%%%% the part above must be pasted in the web page editor to make a calendar widget %%%%%%%%%')
-print('%%%%%%% HTML TABLE %%%%%%%%%')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('%%%%%%% the part above must be pasted in the web page editor to make a calendar WIDGET %%%%%%%%%')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('')
+print('')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('%%%%%%% HTML code for a TABLE %%%%%%%%%')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 print(HTMLTable)
-print('%%%%%%% HTML LIST %%%%%%%%%')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('%%%%%%% HTML code for a LIST %%%%%%%%%')
 print(HTMLList)
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('')
+print('')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+print('%%%%%%% APPSCRIPT variables %%%%%%%%%')
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
 print('%%%%%%% the part below must be pasted in the cals_json list in the script on google %%%%%%%%%')
 
 with open(calendar_json, 'r') as f:
